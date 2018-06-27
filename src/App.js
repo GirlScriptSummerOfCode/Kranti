55bea0be1236d5b134d615d86f9ec79550f6015e
lalitmee <lalitkumar.meena.lk@gmail.com>
2018-06-27 14:18:46 +0530

    created: Basic React Project
    the basic `boilerplate` for react project
    is added to the project.
    
    closes: https://github.com/GirlScriptSummerOfCode/Kranti/issues/1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App" style={{ textAlign: 'center' }}>
+            <h1>Hello World</h1>
+            <p>Welcome to Kranti</p>
+            </div>
+        );
+    }
+}
+
+export default App;
