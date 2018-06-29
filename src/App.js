9808049e613d2292943e0dad28ab0cbf5ad6ba93
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-06-29 12:00:34 +0530

    .js extension removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header.js';
+import Header from './components/Header/Header';
 
 class App extends Component {
     render() {
